55af27d762a1a694d79e42fdcabf4c49f2e61936
DmitryByrigin <dmitri.d907@gmail.com>
2024-03-27 22:00:36 +0100

    Added scene environment

--- Assets/Scripts/RotaryThunderclap.cs
+++ Assets/Scripts/RotaryThunderclap.cs
@@ -5,8 +5,8 @@ public class RotateObject : MonoBehaviour
 {
     public string objectTag = "RotaryThunderclap";
     public float rotationSpeed = 0.5f; // Скорость вращения
-    public Vector3 leftRotation = new Vector3(7.881f, -282.247f, 172.043f); // Позиция при максимальном повороте влево
-    public Vector3 rightRotation = new Vector3(7.881f, -340.387f, 172.043f); // Позиция при максимальном повороте вправо
+    public Vector3 leftRotation = new Vector3(2.289f, 176.043f, 182.022f); // Позиция при максимальном повороте влево
+    public Vector3 rightRotation = new Vector3(2.607f, 103.147f, 178.408f); // Позиция при максимальном повороте вправо
 
     void Start()
     {
