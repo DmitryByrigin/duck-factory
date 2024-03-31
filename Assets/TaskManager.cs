6249bc970c89bbc83d6df7fba6be5add309e9b74
Artem Ponomarov <100004793+Aspergillusplay@users.noreply.github.com>
2024-03-31 13:03:32 +0200

    Adding ATM, electrical panel and fuses

--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -1,40 +1,26 @@
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class TaskManager : MonoBehaviour
 {
-    public string taskObjectName = "Text"; // Имя вашего объекта Text
-    private Text taskText; // Ссылка на текстовый компонент вашего табло
-    private int boxCount = 2; // Устанавливаем количество объектов с тегом Box равным 2
+    public TextMeshProUGUI taskText; // Ссылка на текстовый компонент вашего табло
+    public int boxCount = 2; // Устанавливаем количество объектов с тегом Box равным 0
 
     void Start()
     {
-        // Находим объект Text
-        GameObject taskObject = GameObject.Find(taskObjectName);
-        if (taskObject != null)
-        {
-            taskText = taskObject.GetComponent<Text>();
-        }
-
-        // Если компонент Text не найден, выводим ошибку
-        if (taskText == null)
-        {
-            Debug.LogError("Task Text component not found on object " + taskObjectName);
-            return;
-        }
-
         // Изначально задаем текст задания
         taskText.text = "1. Task, clear the room.";
     }
 
     void Update()
     {
-        // Если количество активных объектов с тегом Box стало меньше boxCount
-        if (Box.ActiveBoxes.Count <= boxCount)
+        // Если количество активных объектов с тегом Box стало меньше или равно boxCount
+        if (GameObject.FindGameObjectsWithTag("Box").Length <= boxCount)
         {
             // Обновляем текст задания
             taskText.text = "1. Task, clear the room.\n2. Task, insert the fuses into the shield.";
-            boxCount = Box.ActiveBoxes.Count; // Обновляем количество активных объектов с тегом Box
+            boxCount = GameObject.FindGameObjectsWithTag("Box").Length; // Обновляем количество активных объектов с тегом Box
         }
     }
 }
